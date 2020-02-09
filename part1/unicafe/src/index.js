75e2f41e56bdc59234099f5433d6edde2f29d6dc
Aurélien Pinson <aurelien.pinson@gmail.com>
2020-02-10 00:12:20 +0100

    exercises(part1): submit unicafe exercise 1.6-1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const DisplayTitle = ({text}) => <h1>{text}</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistic = ({text, value}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
+
+const Button = ({text, onClick}) => (
+    <button onClick={onClick}>{text}</button>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+    if(good === 0 && neutral === 0 && bad === 0) {
+        return <div>No feedback yet</div>
+    }
+
+    return (
+    <>
+        <table>
+            <tbody>
+                <Statistic text="good" value={good} />
+                <Statistic text="neutral" value={neutral} />
+                <Statistic text="bad" value={bad} />
+                <Statistic text="all" value={good + neutral + bad} />
+                <Statistic text="average" value={(good - bad) / (good + neutral + bad)} />
+                <Statistic text="positive" value={(good) * 100 / (good + neutral + bad) + " %"} />
+            </tbody>
+        </table>
+    </>
+    )
+}
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+      <div>
+        <DisplayTitle text="Give feedback" />
+        <Button text="Good" onClick={() => setGood(good + 1)} />
+        <Button text="Neutral" onClick={() => setNeutral(neutral + 1)} />
+        <Button text="Bad" onClick={() => setBad(bad + 1)} />
+        <DisplayTitle text="Statistics :" />
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+    )
+  }
+
+  ReactDOM.render(<App />,
+    document.getElementById('root')
+  )
